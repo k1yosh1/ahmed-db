a24e8c7ad6cf55da522c67041c2fa72a03e83fdb
k1yosh1 <38115179+k1yosh1@users.noreply.github.com>
2018-04-05 23:34:38 +0300

    Update lesson2.sql

--- lesson2.sql
+++ lesson2.sql
@@ -5,3 +5,34 @@ NAME NVARCHAR2(50),
 MANUFACTURER_NAME NVARCHAR2(50),
 COST_PRICE NUMBER
 );
+
+--INSERT
+INSERT INTO PRODUCT
+VALUES(1, 'test1', 'manufact1', 10);
+
+INSERT INTO PRODUCT
+VALUES(2, 'test2', 'manufact1', 20);
+
+INSERT INTO PRODUCT
+VALUES(3, 'test3', 'manufact1', 30);
+
+INSERT INTO PRODUCT
+VALUES(4, 'test4', 'manufact1', 40);
+
+INSERT INTO PRODUCT
+VALUES(5, 'test5', 'manufact1', 50);
+
+INSERT INTO PRODUCT
+VALUES(6, 'test6', 'manufact1', 60);
+
+INSERT INTO PRODUCT
+VALUES(7, 'test7', 'manufact1', 70);
+
+INSERT INTO PRODUCT
+VALUES(8, 'test8', 'manufact1', 80);
+
+INSERT INTO PRODUCT
+VALUES(9, 'test9', 'manufact1', 90);
+
+INSERT INTO PRODUCT
+VALUES(10, 'test10', 'manufact1', 100);
